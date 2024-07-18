8a0860fea6f88bbc02df865006c4b071e3c42637
Colin Platt <colingplatt@gmail.com>
2024-07-18 12:04:55 +0200

    updated webhook

--- netlify/functions/skullBot.js
+++ netlify/functions/skullBot.js
@@ -1,21 +1,18 @@
 const axios = require('axios');
-const { Telegraf } = require('telegraf');
-require('dotenv').config({ path: '.env' }); // Load .env file correctly
-const botapiToken = process.env.TELEGRAM_BOT_API_TOKEN; // Access the environment variable
+const { telegraf } = require('telegraf');
+require('dotenv').config({ path: '.env' });
 
-// Create a Telegram bot instance
-const bot = new Telegraf(botapiToken);
+const botapiToken = process.env.TELEGRAM_BOT_API_TOKEN;
 
-// Handle '/start' command
+const bot = new telegraf(botapiToken);
+
 bot.start((ctx) => ctx.reply('Welcome to skull 💀 price bot'));
 
-// Handle '/price' command
 bot.command('price', async (ctx) => {
   const url = "https://backend.unicorn.meme/market/price/factory%2Funicorn1rn9f6ack3u8t3ed04pfaqpmh5zfp2m2ll4mkty%2Fuskull?frame=1h";
   try {
     const response = await axios.get(url);
     const responseData = response.data;
-    // Assuming responseData is a string
     let price = parseFloat(responseData); 
     ctx.reply(`💀 ${price.toFixed(4)}`);
   } catch (err) {
@@ -24,5 +21,17 @@ bot.command('price', async (ctx) => {
   }
 });
 
-// Launch the Telegram bot
 bot.launch();
+
+module.exports = async (event, context) => {
+  const { message } = JSON.parse(event.body);
+
+  await bot.handleUpdate(message.update_id, message.message); // Handle incoming messages
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      result: 'Message processed successfully',
+    }),
+  };
+};
